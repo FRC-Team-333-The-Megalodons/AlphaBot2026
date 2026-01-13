fba0ecc0d3d19d7c66782b80683c15bfc8c03529
Treayn <Treayn@gmail.com>
2026-01-13 14:01:34 -0500

    Add debugging & interfaces

---
+++ src/main/java/frc/robot/interfaces/Automatable.java
@@ -0,0 +1,22 @@
+package frc.robot.interfaces;
+
+/**
+ * Any subsystem or IO interface which requires Feedback Control,
+ *  should implement or extend this interface & it's methods.
+ */
+public interface Automatable {
+
+  /**
+   * Lift, extend, or rotate a mechanism to a desired target.
+   *
+   * @param setpoint the target angle or distance of the mechanism.
+   */
+  public void moveTo(double setpoint);
+
+  /**
+   * Check if the mechanism has reached the target.
+   *
+   * @return A boolean, indicating if the target has been reached.
+   */
+  public boolean atTarget();
+}
