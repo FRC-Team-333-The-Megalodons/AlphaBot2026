fff45d50e1e2f2d2bd980c076fbb0ed63be16023
SuprezaHakim <hakimjon.boev@gmail.com>
2026-01-24 01:02:13 -0500

    Intake Subsystem completed

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOKraken.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class IntakeIOKraken implements IntakeIO {
+    private final TalonFX motor = new TalonFX(IntakeConstants.MOTOR_ID);
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {
+        inputs.appliedVolts = motor.getMotorVoltage().getValueAsDouble();
+        inputs.currentAmps = motor.getStatorCurrent().getValueAsDouble();
+        inputs.velocityRps = motor.getVelocity().getValueAsDouble();
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        motor.setControl(new VoltageOut(volts));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOKrakenSim.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
+public class IntakeIOKrakenSim implements IntakeIO {
+  private static final LinearSystem<N1, N1, N1> intakePlant = 
+      LinearSystemId.createFlywheelSystem(DCMotor.getKrakenX60(1), 0.001, 1.0);
+ 
+  private FlywheelSim sim = new FlywheelSim(intakePlant, DCMotor.getKrakenX60(1), 0.001);
+  private double appliedVolts = 0.0;
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    sim.update(0.02);
+    inputs.appliedVolts = appliedVolts;
+    inputs.velocityRps = sim.getAngularVelocityRPM() / 60.0;
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    appliedVolts = volts;
+    sim.setInputVoltage(volts);
+  }
+}
