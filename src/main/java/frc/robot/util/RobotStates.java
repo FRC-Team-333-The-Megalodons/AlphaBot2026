cce5342185028be58c3071c21cf48cbaab8f0dca
SuprezaHakim <hakimjon.boev@gmail.com>
2026-01-30 12:56:44 -0500

    added the robot states(needs more states when we add sensors)

--- src/main/java/frc/robot/util/RobotStates.java
+++ src/main/java/frc/robot/util/RobotStates.java
@@ -1,3 +1,26 @@
 package frc.robot.util;
 
-public class RobotStates {}
+public class RobotStates {
+    public static Mode currentMode = Mode.DISABLED;
+    public static GamePieceState pieceState = GamePieceState.NONE;
+    public static FieldLocation location = FieldLocation.ALLIANCE_ZONE;
+    public enum Mode{
+        DISABLED,
+        TELEOP,
+        AUTO,
+        TEST
+    }
+    public enum GamePieceState{
+        NONE,
+        INTAKING,
+        HAS_PIECE,
+        SCORING
+    }
+    public enum FieldLocation{
+        ALLIANCE_ZONE,
+        NEUTRAL_ZONE,
+        OPP_ALLINACE_ZONE
+    }
+}
+
+
