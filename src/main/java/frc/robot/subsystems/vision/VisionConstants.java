bf93245f9d091bab2a22a46b4de09d1dacda4218
SuprezaHakim <hakimjon.boev@gmail.com>
2026-02-07 14:17:43 -0500

    limelight location

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -26,7 +26,7 @@ public class VisionConstants {
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.17145, 0.404, 0.3556, new Rotation3d(0.0, 0, 0.0));
+      new Transform3d(-.2713, -0.281, 0.41539, new Rotation3d(0.0, 0.174533, 0.0));
   public static Transform3d robotToCamera1 =
       new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
   public static Transform3d turretToCamera =
