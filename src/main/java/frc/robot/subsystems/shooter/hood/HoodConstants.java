b41a6e9b66ccce647cf861af45ee6ee234fd72cd
SuprezaHakim <hakimjon.boev@gmail.com>
2026-04-20 09:18:11 -0400

    got rid of the hood.

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.shooter.hood;
-
-public class HoodConstants {
-
-  public static final int MOTOR_ID = 6;
-  public static final double GEAR_RATIO = 15.0;
-  public static final double kP = 25.0;
-}
